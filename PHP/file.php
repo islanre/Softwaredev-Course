066eb91572d8d0faed6cd59d4b3163c5e3914b11
LanJosh1 <o.isijola1@rgu.ac.uk>
2023-02-28 13:00:19 +0000

    update

---
+++ PHP/file.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    </head>
+    <body>
+        <p>
+            <?php
+                echo "Hello World";
+            ?>
+        </p>
+    </body>
+</html>
---
+++ PHP/g.php
@@ -0,0 +1,10 @@
+<?php
+print ("hello");
+echo "heloooo";
+$a=5;
+$b=10;
+$c=$a+$b;
+echo $c;
+
+echo 2*10;
+?>
